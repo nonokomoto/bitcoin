e65fe443d5b3e2bcfa1a07dc62642d01c06c27dc
nonokomoto <ney.carvalho1@hotmail.com>
2022-12-17 22:46:11 +0100

    Atualizar css file

--- script.js
+++ script.js
@@ -16,3 +16,29 @@ window.addEventListener("scroll", reveal);
 
 // To check the scroll position on page load
 reveal();
+
+// JavaScript code
+// Get the list of categories and the container for the posts
+const categories = document.getElementById("categorias");
+const postsContainer = document.getElementById("posts");
+
+// Add a click event listener to the categories list
+categories.addEventListener("click", function(event) {
+  // Get the clicked category
+  const clickedCategory = event.target;
+
+  // Get the category id from the data-id attribute of the clicked category
+  const categoryId = clickedCategory.getAttribute("data-id");
+
+  // Get all the posts
+  const allPosts = postsContainer.querySelectorAll(".card");
+
+  // Loop through the posts and hide the ones that don't have the same category id as the clicked category
+  allPosts.forEach(function(post) {
+    if (post.getAttribute("data-categorias") !== categoryId) {
+      post.style.display = "none";
+    } else {
+      post.style.display = "";
+    }
+  });
+});
